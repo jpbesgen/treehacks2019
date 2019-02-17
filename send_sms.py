b867899a4b034993c09b666fdaf12a7c4d287130
Makena <mfetzer@berkeley.edu>
2019-02-16 17:36:01 -0800

    send_sms functional and populated by hidden passwords file in gitignor

---
+++ send_sms.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+from passwords import *
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = TWILIO_ACCOUNT_SID
+auth_token = TWILIO_TOKEN
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body= "You are designated as " + USER_NAME + "'s emergency contact. \
+                     They were stopped at " + STOP_TIME + " in " + STOP_LOCATION + ".",
+                     from_='+18059792058',
+                     to=EMERGENCY_CONTACT
+                 )
+
+print(message.sid)
